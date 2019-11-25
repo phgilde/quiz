3ea33eedb6bf4cb86b3afcb7bf259d93c0a9fffd
phgilde <ph.gilde@gmail.com>
2019-11-25 16:31:03 +0100

    gen_quizform submit text

--- app/forms.py
+++ app/forms.py
@@ -5,17 +5,17 @@ from wtforms.validators import DataRequired, Length
 
 class QuizForm(FlaskForm):
     name = StringField("Dein Name", validators=[DataRequired(), Length(max=15)])
-    submit = SubmitField("Jetzt Quiz erstellen!")
+    # submit = SubmitField("Jetzt Quiz erstellen!")
 
 
-def gen_quizform(questions, answers, submit):
+def gen_quizform(questions, answers, submit_text):
     class QuizFormClone(QuizForm):
-        pass
+        submit = SubmitField(submit_text)
 
     for i in range(len(questions)):
         field = RadioField(label=questions[i],
                            choices=[(str(x), answers[i][x])
                                     for x in range(len(answers[i]))])
         setattr(QuizFormClone, f"field{i}", field)
-
+    # setattr(QuizFormClone, "submit", SubmitField(submit))
     return QuizFormClone()
