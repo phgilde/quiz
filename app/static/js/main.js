71d592f9886a9725014da0c6c425cd877128e570
pgmcr <ph.gilde@gmail.com>
2019-11-28 18:13:23 +0100

    copy to clipboard

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,5 +1,6 @@
-$("#clipboard").click(function() {
-    $("#clipboard")[0].select();
-    $("#clipboard")[0].setSelectionRange(0, 99999);
+$("#clipboard").click(function myFunction() {
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
     document.execCommand("copy");
 })
