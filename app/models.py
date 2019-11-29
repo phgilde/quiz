695bcb3762cf4053adb444a4d3f432c3310d57df
pgmcr <ph.gilde@gmail.com>
2019-11-29 20:56:06 +0100

    string columns have fixed length

--- app/models.py
+++ app/models.py
@@ -1,17 +1,18 @@
 from app import db
+from app.quiz import answers
 
 
 class Quiz(db.Model):
-    name = db.Column(db.String)
-    correct_answers = db.Column(db.String)
+    name = db.Column(db.String(length=255))
+    correct_answers = db.Column(db.String(length=len(answers)*2))
     id_ = db.Column(db.Integer, primary_key=True)
     answers = db.relationship("Answer", backref="quiz", lazy="dynamic")
 
 
 class Answer(db.Model):
     id_ = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String)
-    answers = db.Column(db.String)
+    name = db.Column(db.String(length=255))
+    answers = db.Column(db.String(length=len(answers)*2))
     quiz_id = db.Column(db.Integer, db.ForeignKey("quiz.id_"))
 
     def score(self):
