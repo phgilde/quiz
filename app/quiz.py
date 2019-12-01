d0768968b0fc5729923c29b6d4a68d1ac43e2742
pgmcr <ph.gilde@gmail.com>
2019-12-01 15:05:46 +0100

    new questions and answers for quiz

--- app/quiz.py
+++ app/quiz.py
@@ -4,7 +4,9 @@ questions = [
     "Lebensziel",
     "Freizeitbeschäftigung",
     "Beruf",
-
+    "Lieblingsgetränk (kalt)",
+    "Lieblingsgetränk (heiß)",
+    "Jahreszeit"
 ]
 answers = [
     [
@@ -35,6 +37,47 @@ answers = [
         "Büro",
         "Baustelle",
         "Koch",
-        "Künstler"
+        "Künstler",
+        "Sozial"
+    ],
+    [
+        "Cola",
+        "Wasser",
+        "Eistee",
+        "Saft",
+        "Limonade"
+    ],
+    [
+        "Tee",
+        "Kaffee",
+        "Heiße Schokolade"
+    ],
+    [
+        "Winter",
+        "Frühling",
+        "Sommer",
+        "Herbst"
     ]
+]
+
+questiontexts_new = [
+    "Was würdest du am liebsten essen?",
+    "Wo würdest du am liebsten Urlaub machen?",
+    "Was ist dir am wichtigsten?",
+    "Was würdest du in deiner Freizeit am liebsten tun?",
+    "In welchem Berufsfeld würdest du am liebsten Arbeiten?",
+    "Wie erfrischst du dich im Sommer?",
+    "Wie wärmst du dich im Winter auf?",
+    "Welche Jahreszeit magst du am Liebsten?"
+]
+
+questiontexts_name = [
+    "Was würde {} am liebsten essen?",
+    "Wo würde {} am liebsten Urlaub machen?",
+    "Was ist dir am wichtigsten?",
+    "Was würde {} in seiner/ihrer Freizeit am liebsten tun?",
+    "In welchem Berufsfeld würde {} am liebsten Arbeiten?",
+    "Wie erfrischst du dich im Sommer?",
+    "Wie wärmt {} sich im Winter auf?",
+    "Welche Jahreszeit mag {} am Liebsten?"
 ]
