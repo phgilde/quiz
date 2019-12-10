791b0a07f763bb7e8543fadfd5f8ab9c6cb8b8f6
pgmcr <ph.gilde@gmail.com>
2019-12-10 20:16:37 +0100

    added file for newquiz code

---
+++ app/static/js/newquiz.js
@@ -0,0 +1,43 @@
+run = function (questions, answers) {
+    $("form").hide();
+    var result = []
+    var i = 0;
+    var name = "";
+    updateQuiz = function(clicked) {
+        return function(){
+
+            result.push(clicked);
+            $("#answers").empty();
+            $("#question").empty();
+
+            if(i==questions.length) {
+                $("form").show();
+                $("form :text").hide();
+                $("form #name").val(name);
+                $("form #answers").val(result.join(" "));
+            }
+
+            $("#question").html(questions[i]);
+            for(var j = 0; j<answers[i].length; j++) {
+                $("#answers").append($('<button class="answers btn btn-info btn-lg"></button>').html(answers[i][j]).click(updateQuiz(answers[i][j])));
+            }
+
+
+            i += 1;
+
+        }
+    }
+    nameSubmitFunc = function() {
+        if ($("#name").val().length > 1) {
+            $("#name-label").hide();
+            $("#name").hide();
+            $("#send-name").hide();
+            name = $("#name").val();
+            updateQuiz(0)();
+            result.pop()
+        } else {
+            alert("Bitte gebe einen Namen ein");
+        }
+    }
+    $("#send-name").click(nameSubmitFunc);
+}
