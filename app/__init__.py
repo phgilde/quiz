f57aa6698ac32a1777a54a338ccaa904d2f1472b
pgmcr <ph.gilde@gmail.com>
2019-12-18 21:53:35 +0100

    mail logging

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import logging
+from logging.handlers import SMTPHandler
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -9,3 +12,18 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db, compare_type=True)
 
 from app import routes
+
+
+if not app.debug:
+    if app.config["MAIL_SERVER"]:
+        auth = None
+        if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+            auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+        secure = None
+        if app.config["MAIL_USE_TLS"]:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
+            fromaddr="logging@"+app.config["MAIL_SERVER"],
+            to_addrs=app.config["ADMINS"], subject="MyQuiz Fehler"
+        )
