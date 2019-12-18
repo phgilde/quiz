b266da979d71e2fec73269a2a32c83f67538d627
pgmcr <ph.gilde@gmail.com>
2019-12-18 23:07:01 +0100

    s

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "key"
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")  # or "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")  or "sqlite:///" + os.path.join(basedir, "app.db")
     print(SQLALCHEMY_DATABASE_URI, file=sys.stderr)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ANSWERS_PER_PAGE = 10
