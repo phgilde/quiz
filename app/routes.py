bdd4a03074cd52142f782609aa548aca57aaf16f
pgmcr <ph.gilde@gmail.com>
2020-10-06 17:49:53 +0200

    corona

--- app/routes.py
+++ app/routes.py
@@ -142,3 +142,8 @@ def quizanswers(id_):
             name=quiz.name,
             max_score=len(questions),
         )
+
+
+@app.route("/corona-lockdown-news")
+def corona():
+    return render_template("corona.html")
