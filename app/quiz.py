bdbf70c6a3d95fcff3ee9fd3d10da3ea92b892b0
pgmcr <ph.gilde@gmail.com>
2020-10-10 18:10:35 +0200

    grammatik

--- app/quiz.py
+++ app/quiz.py
@@ -6,58 +6,17 @@ questions = [
     "Beruf",
     "Lieblingsgetränk (kalt)",
     "Lieblingsgetränk (heiß)",
-    "Jahreszeit"
+    "Jahreszeit",
 ]
 answers = [
-    [
-        "Pizza",
-        "Salat",
-        "Nudeln",
-        "Döner"
-    ],
-    [
-        "Südsee",
-        "Gebirge",
-        "Stadt",
-        "Regenwald"
-    ],
-    [
-        "Freunde",
-        "Familie",
-        "Liebe",
-        "Geld"
-    ],
-    [
-        "Sport machen",
-        "mit Freunden treffen",
-        "Computer spielen",
-        "Schlafen"
-    ],
-    [
-        "Büro",
-        "Baustelle",
-        "Koch",
-        "Künstler",
-        "Sozial"
-    ],
-    [
-        "Cola",
-        "Wasser",
-        "Eistee",
-        "Saft",
-        "Limonade"
-    ],
-    [
-        "Tee",
-        "Kaffee",
-        "Heiße Schokolade"
-    ],
-    [
-        "Winter",
-        "Frühling",
-        "Sommer",
-        "Herbst"
-    ]
+    ["Pizza", "Salat", "Nudeln", "Döner"],
+    ["Südsee", "Gebirge", "Stadt", "Regenwald"],
+    ["Freunde", "Familie", "Liebe", "Geld"],
+    ["Sport machen", "mit Freunden treffen", "Computer spielen", "Schlafen"],
+    ["Büro", "Baustelle", "Koch", "Künstler", "Sozial"],
+    ["Cola", "Wasser", "Eistee", "Saft", "Limonade"],
+    ["Tee", "Kaffee", "Heiße Schokolade"],
+    ["Winter", "Frühling", "Sommer", "Herbst"],
 ]
 
 questiontexts_new = [
@@ -68,7 +27,7 @@ questiontexts_new = [
     "In welchem Berufsfeld würdest du am liebsten Arbeiten?",
     "Wie erfrischst du dich im Sommer?",
     "Wie wärmst du dich im Winter auf?",
-    "Welche Jahreszeit magst du am Liebsten?"
+    "Welche Jahreszeit magst du am Liebsten?",
 ]
 
 questiontexts_name = [
@@ -77,7 +36,7 @@ questiontexts_name = [
     "Was ist dir am wichtigsten?",
     "Was würde {} in seiner/ihrer Freizeit am liebsten tun?",
     "In welchem Berufsfeld würde {} am liebsten Arbeiten?",
-    "Wie erfrischst du dich im Sommer?",
+    "Wie erfrischst {} sich im Sommer?",
     "Wie wärmt {} sich im Winter auf?",
-    "Welche Jahreszeit mag {} am Liebsten?"
+    "Welche Jahreszeit mag {} am Liebsten?",
 ]
