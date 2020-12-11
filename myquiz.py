99a843a149462067fd446e6fb3c16b77260f05a5
pgmcr <ph.gilde@gmail.com>
2020-12-11 18:39:15 +0100

    shell context

--- myquiz.py
+++ myquiz.py
@@ -1,1 +1,15 @@
-from app import app
+from app import app, db
+from app.models import Quiz, Answer, Question, Guess, AnswerGuess
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        "db": db,
+        "Quiz": Quiz,
+        "Answer": Answer,
+        "Question": Question,
+        "Guess": Guess,
+        "AnswerGuess": AnswerGuess,
+    }
+
