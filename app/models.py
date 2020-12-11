29e93451c021fda9c0d9a9e7204689629f345b3c
pgmcr <ph.gilde@gmail.com>
2020-12-11 18:33:50 +0100

    added creation date to quiz

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from app import db
 import random
 from app import app
+from datetime import date
 
 
 def id_gen():
@@ -12,6 +13,7 @@ class Quiz(db.Model):
     name = db.Column(db.String(length=255))
     questions = db.relationship("Question", backref="quiz", lazy="dynamic")
     guesses = db.relationship("Guess", backref="quiz", lazy="dynamic")
+    creation_date = db.Column(db.Date, default=date)
 
 
 class Question(db.Model):
@@ -28,7 +30,9 @@ class Answer(db.Model):
     text = db.Column(db.String(length=255))
     question_id = db.Column(db.BigInteger, db.ForeignKey("question.id_"))
     correct_answer = db.Column(db.Boolean)
-    answer_guesses = db.relationship("AnswerGuess", backref="answer", lazy="dynamic")
+    answer_guesses = db.relationship(
+        "AnswerGuess", backref="answer", lazy="dynamic"
+    )
     image = db.Column(db.String(length=255))
 
 
@@ -36,7 +40,9 @@ class Guess(db.Model):
     id_ = db.Column(db.BigInteger, primary_key=True, default=id_gen)
     quiz_id = db.Column(db.BigInteger, db.ForeignKey("quiz.id_"))
     name = db.Column(db.String(length=255))
-    answer_guesses = db.relationship("AnswerGuess", backref="guess", lazy="dynamic")
+    answer_guesses = db.relationship(
+        "AnswerGuess", backref="guess", lazy="dynamic"
+    )
 
     def score(self):
         result = 0
